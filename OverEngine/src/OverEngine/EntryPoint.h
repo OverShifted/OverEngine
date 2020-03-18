f7082f3f7bf8311bfce9d1639b13b5606fdeaf96
OverShifted <prowidgs@gmail.com>
2020-03-18 17:21:52 +0000

    Making Logger and reGen project

--- OverEngine/src/OverEngine/EntryPoint.h
+++ OverEngine/src/OverEngine/EntryPoint.h
@@ -1,14 +1,14 @@
 #pragma once
 #ifdef OE_PLATFORM_WINDOWS
 
-#include <iostream>
 extern  OverEngine::Application* OverEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
 	OverEngine::Log::Init();
+	OE_CORE_INFO("OverEngine Yay!");
+
 	auto app = OverEngine::CreateApplication();
-	std::cout << "salam";
 	app->Run();
 	delete app;
 }
