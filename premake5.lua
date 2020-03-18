7ae28ecc55cef622e803b65fe3b638f72f6184e6
OverShifted <prowidgs@gmail.com>
2020-03-18 13:53:13 +0000

    add Premake build system

---
+++ premake5.lua
@@ -0,0 +1,328 @@
+workspace "OverEngine"
+	architecture "x64"
+	configurations
+	{
+		"Debug",
+		"Realease",
+		"Dist"
+	}
+
+--            debug/realease       OS              x64
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "OverEngine"
+	location "OverEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+ 	}
+
+	-- include { "%{prj.name}/vendor/spdlog/include" }
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.18362.0"
+
+		defines
+		{
+			"OE_PLATFORM_WINDOWS",
+			"OE_BUILD_DYNAMIC"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/OverPlayerExec")--,
+			--("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/OverEditorExec")
+		}
+
+	filter "configurations:Debug"
+		defines "OE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Realease"
+		defines "OE_REALEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OE_DIST"
+		optimize "On"
+
+
+
+project "OverPlayerExec"
+	location "OverPlayerExec"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"OverEngine/src"
+	}
+
+	links
+	{
+		"OverEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.18362.0"
+
+		defines
+		{
+			"OE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "OE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Realease"
+		defines "OE_REALEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OE_DIST"
+		optimize "On"
+
+
+project "OverEditor"
+	location "OverEditor"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	-- include {  }
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.18362.0"
+
+		defines
+		{
+			"OE_PLATFORM_WINDOWS",
+			"OE_BUILD_DYNAMIC"
+		}
+
+		-- postbuildcommands
+		-- {
+		-- 	("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/OverEditorExec")
+		-- }
+	filter "configurations:Debug"
+		defines "OE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Realease"
+		defines "OE_REALEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OE_DIST"
+		optimize "On"
+
+--
+
+project "OverEditorExec"
+	location "OverEditorExec"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	--includedirs { "OverEngine/src" }
+
+	links
+	{
+		"OverEngine",
+		"OverEditor"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.18362.0"
+
+		defines
+		{
+			"OE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "OE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Realease"
+		defines "OE_REALEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OE_DIST"
+		optimize "On"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- workspace "OverEngine"
+-- 	architecture "x64"
+
+-- 	configurations
+-- 	{
+-- 		"Debug",
+-- 		"Release",
+-- 		"Dist"
+-- 	}
+
+-- outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+
+-- project "OverEngine"
+-- 	location "OverEngine"
+-- 	kind "SharedLib"
+-- 	language "C++"
+
+-- 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+-- 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+-- 	files
+-- 	{
+-- 		"%{prj.name}/src/**.h",
+-- 		"%{prj.name}/src/**.cpp"
+-- 	}
+
+	-- includedirs
+	-- {
+	-- 	"%{prj.name}/src"
+	-- }
+
+	-- links
+	-- {
+	-- 	"GLFW",
+	-- 	"opengl32.lib"
+	-- }
+
+	-- filter "system:windows"
+	-- 	cppdialect "C++17"
+	-- 	staticruntime "On"
+	-- 	systemversion "10.0.18362.0"
+
+	-- 	defines
+	-- 	{
+	-- 		"OE_PLATFORM_WINDOWS",
+	-- 		"OE_BUILD_DYNAMIC"
+	-- 	}
+
+	-- 	postbuildcommands
+	-- 	{
+	-- 		("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/OverPlayerExec")
+	-- 	}
+
+	-- filter "configurations:Debug"
+	-- 	defines "OE_DEBUG"
+	-- 	symbols "On"
+
+	-- filter "configurations:Release"
+	-- 	defines "OE_RELEASE"
+	-- 	optimize "On"
+
+	-- filter "configurations:Dist"
+	-- 	defines "OE_DIST"
+	-- 	optimize "On"
+
+-- project "OverPlayerExec"
+-- 	location "OverPlayerExec"
+-- 	kind "ConsoleApp"
+-- 	language "C++"
+
+-- 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+-- 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+-- 	files
+-- 	{
+-- 		"%{prj.name}/src/**.h",
+-- 		"%{prj.name}/src/**.cpp"
+-- 	}
+
+-- 	includedirs
+-- 	{
+-- 		"OverEngine/src"
+-- 	}
+
+-- 	links
+-- 	{
+-- 		"OverEngine"
+-- 	}
+
+-- 	filter "system:windows"
+-- 		cppdialect "C++17"
+-- 		staticruntime "On"
+-- 		systemversion "10.0.18362.0"
+
+-- 		defines
+-- 		{
+-- 			"OE_PLATFORM_WINDOWS"
+-- 		}
+
+-- 	filter "configurations:Debug"
+-- 		defines "OE_DEBUG"
+-- 		symbols "On"
+
+-- 	filter "configurations:Release"
+-- 		defines "OE_RELEASE"
+-- 		optimize "On"
+
+-- 	filter "configurations:Dist"
+-- 		defines "OE_DIST"
+-- 		optimize "On"
