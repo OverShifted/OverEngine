0146021e0235de0de2e33dba5a089ed93c7b078d
OverShifted <prowidgs@gmail.com>
2020-03-27 17:13:40 +0000

    Update Event.h to make Event handled state public
    Later will be used in Layer system

--- OverEngine/src/OverEngine/Events/Event.h
+++ OverEngine/src/OverEngine/Events/Event.h
@@ -38,8 +38,9 @@ namespace OverEngine {
 
 	class OVER_API Event
 	{
-		friend class EventDispatcher;
 	public:
+		bool Handled = false;
+
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryFlags() const = 0;
@@ -49,8 +50,6 @@ namespace OverEngine {
 		{
 			return GetCategoryFlags() & category;
 		}
-	protected:
-		bool m_Handled = false;
 	};
 
 	class EventDispatcher
@@ -68,7 +67,7 @@ namespace OverEngine {
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.m_Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(*(T*)&m_Event);
 				return true;
 			}
 			return false;
