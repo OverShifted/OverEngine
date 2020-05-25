c801e3286a68f08a65cafe2903e81933dc1394a2
OverShifted <prowidgs@gmail.com>
2020-05-25 19:08:38 +0430

    change #if defined to #ifdef and better empty OE_ASSERT in Core.h

--- OverEngine/src/OverEngine/Core/Core.h
+++ OverEngine/src/OverEngine/Core/Core.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#if defined OE_PLATFORM_WINDOWS
-	#if defined OE_BUILD_SHARED
-		#if defined OE_PROJECT_BUILD_SHARED
+
+#ifdef OE_PLATFORM_WINDOWS
+	#ifdef OE_BUILD_SHARED
+		#ifdef OE_PROJECT_BUILD_SHARED
 			#define OVER_API __declspec(dllexport)
 		#else
 			#define OVER_API __declspec(dllimport)
@@ -12,9 +13,9 @@
 	#endif // OE_BUILD_SHARED
 #endif // OE_PLATFORM_WINDOWS
 
-#if defined OE_PLATFORM_LINIUX
-	#if defined OE_BUILD_SHARED
-		#if defined OE_PROJECT_BUILD_SHARED
+#ifdef OE_PLATFORM_LINIUX
+	#ifdef OE_BUILD_SHARED
+		#ifdef OE_PROJECT_BUILD_SHARED
 			#define OVER_API __attribute__((visibility("default")))
 		#else
 			#define OVER_API
@@ -24,25 +25,27 @@
 	#endif // OE_BUILD_SHARED
 #endif // OE_PLATFORM_LINIUX
 
-#if defined OE_DEBUG
+#ifdef OE_DEBUG
 
 	// debugbreak
-	#if defined OE_PLATFORM_WINDOWS
-		#define OE_DEBUGBREAK() __debugbreak()
+	#ifdef OE_PLATFORM_WINDOWS
+		#define OE_DEBUGBREAK __debugbreak()
 	#elif defined OE_PLATFORM_LINUX
 		#include <signal.h>
-		#define OE_DEBUGBREAK() raise(SIGTRAP)
+		#define OE_DEBUGBREAK raise(SIGTRAP)
 	#endif // End of debugbreak
 
 	#define OE_ENABLE_ASSERTS
+#else
+	#define OE_DEBUGBREAK
 #endif // OE_DEBUG
 
-#if defined OE_ENABLE_ASSERTS
-	#define OE_ASSERT(x, ...) { if(!(x)) { OE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-	#define OE_CORE_ASSERT(x, ...) { if(!(x)) { OE_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#ifdef OE_ENABLE_ASSERTS
+	#define OE_ASSERT(x, ...) { if(!(x)) { OE_ERROR("Assertion Failed: {0}", __VA_ARGS__); OE_DEBUGBREAK; } }
+	#define OE_CORE_ASSERT(x, ...) { if(!(x)) { OE_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); OE_DEBUGBREAK; } }
 #else
-	#define OE_ASSERT(x, ...)
-	#define OE_CORE_ASSERT(x, ...)
+	#define OE_ASSERT( ... )
+	#define OE_CORE_ASSERT( ... )
 #endif
 
 #define BIT(x) (1 << x)
