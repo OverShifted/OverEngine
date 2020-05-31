3bc1e93665082a6d4a00f4d351e27640b68340fc
OverShifted <prowidgs@gmail.com>
2020-05-31 23:36:28 +0430

    Create RendererAPI based on using api

--- OverEngine/src/OverEngine/Renderer/RenderCommand.cpp
+++ OverEngine/src/OverEngine/Renderer/RenderCommand.cpp
@@ -5,5 +5,17 @@
 
 namespace OverEngine
 {
-	RendererAPI* RenderCommand::s_RendererAPI = new OpenGLRendererAPI();
+	static RendererAPI* CreateRendererAPI()
+	{
+		switch (RendererAPI::GetAPI())
+		{
+		case RendererAPI::API::None:    OE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return new OpenGLRendererAPI();
+		}
+
+		OE_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+	RendererAPI* RenderCommand::s_RendererAPI = CreateRendererAPI();
 }
