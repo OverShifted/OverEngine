0a4a78d53194ca9d47027978bd2ee79830783fd3
OverShifted <prowidgs@gmail.com>
2020-05-31 17:20:51 +0430

    Add simple Math and Color header files and remove Vector2

---
+++ OverEngine/src/OverEngine/Core/Math/Color.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace OverEngine
+{
+	using Color = glm::vec4;
+}
---
+++ OverEngine/src/OverEngine/Core/Math/Math.h
@@ -0,0 +1,17 @@
+#include <glm/glm.hpp>
+
+#include "Color.h"
+
+namespace OverEngine
+{
+	namespace Math
+	{
+		using Vector1 = glm::vec1;
+		using Vector2 = glm::vec2;
+		using Vector3 = glm::vec3;
+		using Vector4 = glm::vec4;
+
+		using Mat3x3 = glm::mat3;
+		using Mat4x4 = glm::mat4;
+	}
+}
