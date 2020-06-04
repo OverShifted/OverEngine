3d035e5f3492b1f3d1188d2a2b546e30b103f42c
OverShifted <prowidgs@gmail.com>
2020-06-04 11:22:54 +0430

    Missing iterator wrappers in LayerStack.h and rename them from std::vector to vector

--- OverEngine/src/OverEngine/Layers/LayerStack.h
+++ OverEngine/src/OverEngine/Layers/LayerStack.h
@@ -18,10 +18,17 @@ namespace OverEngine {
 		void PopLayer(Layer* layer);
 		void PopOverlay(Layer* overlay);
 
-		std::vector<Layer*>::iterator begin() { return m_Layers.begin(); }
-		std::vector<Layer*>::iterator end() { return m_Layers.end(); }
+		Vector<Layer*>::iterator begin() { return m_Layers.begin(); }
+		Vector<Layer*>::iterator end() { return m_Layers.end(); }
+		Vector<Layer*>::reverse_iterator rbegin() { return m_Layers.rbegin(); }
+		Vector<Layer*>::reverse_iterator rend() { return m_Layers.rend(); }
+
+		Vector<Layer*>::const_iterator begin() const { return m_Layers.begin(); }
+		Vector<Layer*>::const_iterator end()	const { return m_Layers.end(); }
+		Vector<Layer*>::const_reverse_iterator rbegin() const { return m_Layers.rbegin(); }
+		Vector<Layer*>::const_reverse_iterator rend() const { return m_Layers.rend(); }
 	private:
-		std::vector<Layer*> m_Layers;
+		Vector<Layer*> m_Layers;
 		unsigned int m_LayerInsertIndex = 0;
 	};
 
