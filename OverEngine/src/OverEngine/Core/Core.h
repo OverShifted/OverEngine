687da7926f68f9bd3ac8d44e030060a740d95c82
OverShifted <prowidgs@gmail.com>
2020-06-04 10:57:06 +0430

    Added CreateScope and CreateRef

--- OverEngine/src/OverEngine/Core/Core.h
+++ OverEngine/src/OverEngine/Core/Core.h
@@ -59,9 +59,17 @@ namespace OverEngine
 {
 	template<typename T>
 	using Scope = std::unique_ptr<T>;
+	constexpr Scope<T> CreateScope(Args&& ... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
 
 	template<typename T>
 	using Ref = std::shared_ptr<T>;
+	constexpr Ref<T> CreateRef(Args&& ... args)
+	{
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
 
 	template<typename T>
 	using Vector = std::vector<T>;
