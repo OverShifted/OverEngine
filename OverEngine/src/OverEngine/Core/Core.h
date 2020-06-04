51ec2d38c094a50f22a190f2c1fc4e22326805d9
OverShifted <prowidgs@gmail.com>
2020-06-04 17:21:30 +0430

    Added missing template in CreateScope and CreateRef

--- OverEngine/src/OverEngine/Core/Core.h
+++ OverEngine/src/OverEngine/Core/Core.h
@@ -59,6 +59,8 @@ namespace OverEngine
 {
 	template<typename T>
 	using Scope = std::unique_ptr<T>;
+	template<typename T, typename ... Args>
+	
 	constexpr Scope<T> CreateScope(Args&& ... args)
 	{
 		return std::make_unique<T>(std::forward<Args>(args)...);
@@ -66,6 +68,8 @@ namespace OverEngine
 
 	template<typename T>
 	using Ref = std::shared_ptr<T>;
+
+	template<typename T, typename ... Args>
 	constexpr Ref<T> CreateRef(Args&& ... args)
 	{
 		return std::make_shared<T>(std::forward<Args>(args)...);
