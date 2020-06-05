a160a3426e1688ce1110f3b7c161b8928a7976e9
OverShifted <prowidgs@gmail.com>
2020-06-05 11:20:32 +0430

    use new iterators in application class

--- OverEngine/src/OverEngine/Core/Application.cpp
+++ OverEngine/src/OverEngine/Core/Application.cpp
@@ -43,9 +43,9 @@ namespace OverEngine
 		dispatcher.Dispatch<WindowCloseEvent> (OE_BIND_EVENT_FN(Application::OnWindowClose));
 		dispatcher.Dispatch<WindowResizeEvent>(OE_BIND_EVENT_FN(Application::OnWindowResize));
 
-		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); it++)
 		{
-			(*--it)->OnEvent(e);
+			(*it)->OnEvent(e);
 			if (e.Handled)
 				break;
 		}
