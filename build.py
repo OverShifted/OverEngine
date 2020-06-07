3b5be96037b9d3e5287c918279a315a3b2dd9e22
OverShifted <prowidgs@gmail.com>
2020-06-07 13:38:53 +0430

    Add simple python builder script

---
+++ build.py
@@ -0,0 +1,32 @@
+import platform, os, sys
+
+SYSTEM_LINUX   = 0
+SYSTEM_WINDOWS = 1
+
+systemInfo = platform.system()
+if systemInfo == 'Linux':
+    systemInfo = SYSTEM_LINUX
+elif systemInfo == 'Windows':
+    systemInfo = SYSTEM_WINDOWS
+else:
+    print("Unknown OS")
+    quit()
+
+WannaBuild = "-b" in sys.argv[1:] or "--b" in sys.argv[1:] or "--build" in sys.argv[1:] or "-build" in sys.argv[1:]
+
+premakeCommand = "vendor\\bin\\premake\\premake5.exe "
+buildCommand = ""
+if systemInfo == SYSTEM_WINDOWS:
+    premakeCommand += "vs2019"
+    buildCommand = "msbuild"
+elif systemInfo == SYSTEM_LINUX:
+    premakeCommand += "gmake2"
+    buildCommand = "make"
+
+print("Generating project files\n")
+if os.system(premakeCommand) == 0:
+    print("\nProject files generated")
+    if WannaBuild:
+        os.system(buildCommand)
+else:
+    print("\nProject files generation failed")
