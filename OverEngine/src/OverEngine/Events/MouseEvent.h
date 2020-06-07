c3cef141c33f8faddc2f80d5964dc64cd9431b40
OverShifted <prowidgs@gmail.com>
2020-06-07 13:38:29 +0430

    Fix MouseButtonEvent categories

--- OverEngine/src/OverEngine/Events/MouseEvent.h
+++ OverEngine/src/OverEngine/Events/MouseEvent.h
@@ -53,7 +53,7 @@ namespace OverEngine {
 	public:
 		inline int GetMouseButton() const { return m_Button; }
 
-		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryMouseButton | EventCategoryInput)
 	protected:
 		MouseButtonEvent(int button)
 			: m_Button(button) {}
