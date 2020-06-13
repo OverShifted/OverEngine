d0e7cb32e517a2fe81e3673f14f66d109270607d
OverShifted <prowidgs@gmail.com>
2020-06-13 17:29:21 +0430

    Fix single warning

--- OverEngine/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ OverEngine/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -65,7 +65,7 @@ namespace OverEngine
 					ShaderDataTypeToOpenGLBaseType(element.Type),
 					element.Normalized ? GL_TRUE : GL_FALSE,
 					layout.GetStride(),
-					(const void*)element.Offset);
+					(const void*)(intptr_t)element.Offset);
 				index++;
 			}
 
