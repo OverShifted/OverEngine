1bc120d9db103303a556ce8bc5f23775277cb405
OverShifted <prowidgs@gmail.com>
2020-06-16 15:12:01 +0430

    Better Camera Impl ***** this is a complete commit and don't have any error

--- OverEngine/src/OverEngine/Renderer/Camera.cpp
+++ OverEngine/src/OverEngine/Renderer/Camera.cpp
@@ -10,42 +10,66 @@ namespace OverEngine
 		/// Camera /////////////////////////////////////////////////////////////////////////////////////////////////////
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		Camera::Camera(Math::Mat4x4 viewMat, Math::Mat4x4 projMat)
-			: m_ViewMat(viewMat), m_ProjectionMat(projMat), m_ViewProjectionMat(m_ProjectionMat * m_ViewMat)
+		Camera::Camera(Math::Mat4x4 viewMat, Math::Mat4x4 projMat, CameraType type,
+					   float aspectRatio, float orthoSizeOrFov)
+			: m_ViewMatrix(viewMat), m_ProjectionMatrix(projMat),
+			  m_ViewProjectionMatrix(m_ProjectionMatrix * m_ViewMatrix),
+			  m_Type(type), m_Position(Math::Vector3(0.0f)), m_Rotation(Math::Vector3(0.0f)),
+			  m_AspectRatio(aspectRatio), m_FieldOfView(orthoSizeOrFov), m_OrthographicSize(orthoSizeOrFov)
 		{
 		}
 
 		void Camera::RecalculateViewProjectionMatrix()
 		{
-			m_ViewProjectionMat = m_ProjectionMat * m_ViewMat;
+			m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
+		}
+
+		void Camera::RecalculateViewMatrix()
+		{
+			glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Position) *
+			glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation.x), glm::vec3(1, 0, 0)) *
+			glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation.y), glm::vec3(0, 1, 0)) *
+			glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation.z), glm::vec3(0, 0, 1));
+
+			m_ViewMatrix = glm::inverse(transform);
+			RecalculateViewProjectionMatrix();
 		}
 
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		/// OrthographicCamera /////////////////////////////////////////////////////////////////////////////////////////
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		OrthographicCamera::OrthographicCamera(float size, float aspectRatio)
-			: Camera(Math::Mat4x4(1.0f), glm::ortho(-size * aspectRatio, size* aspectRatio, -size, size)),
-			  m_AspectRatio(aspectRatio), m_Size(size)
+		OrthographicCamera::OrthographicCamera(float size, float aspectRatio, float zNear /*= -1.0f*/, float zFar /*= 1.0f*/)
+			: Camera(
+				Math::Mat4x4(1.0f),
+			    glm::ortho(-size * aspectRatio, size* aspectRatio, -size, size, zNear, zFar),
+			  	CameraType::Orthographic, aspectRatio, size
+			  )
 		{
-		}
-
-
-		void OrthographicCamera::SetOrthographicSize(float size)
-		{
-			m_Size = size;
-			RecalculateProjectionMatrix();
-		}
-
-		void OrthographicCamera::SetAspectRatio(float aspectRatio)
-		{
-			m_AspectRatio = aspectRatio;
-			RecalculateProjectionMatrix();
 		}
 
 		void OrthographicCamera::RecalculateProjectionMatrix()
 		{
-			m_ProjectionMat = glm::ortho(-m_Size * m_AspectRatio, m_Size * m_AspectRatio, -m_Size, m_Size);
+			m_ProjectionMatrix = glm::ortho(-m_OrthographicSize * m_AspectRatio, m_OrthographicSize * m_AspectRatio, -m_OrthographicSize, m_OrthographicSize);
+			RecalculateViewProjectionMatrix();
+		}
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// PerspectiveCamera //////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		PerspectiveCamera::PerspectiveCamera(float fov, float aspectRatio, float zNear /*= 0.05f*/, float zFar /*= 100.0f*/)
+			: Camera(
+				Math::Mat4x4(1.0f),
+				glm::perspective(glm::radians(fov), aspectRatio, zNear, zFar),
+			  	CameraType::Orthographic, aspectRatio, fov
+			  )
+		{
+		}
+
+		void PerspectiveCamera::RecalculateProjectionMatrix()
+		{
+			m_ProjectionMatrix = glm::perspective(glm::radians(m_FieldOfView), m_AspectRatio, 0.1f, 100.0f);
 			RecalculateViewProjectionMatrix();
 		}
 
--- OverEngine/src/OverEngine/Renderer/Camera.h
+++ OverEngine/src/OverEngine/Renderer/Camera.h
@@ -4,43 +4,76 @@ namespace OverEngine
 {
 	namespace Renderer
 	{
+		enum class CameraType
+		{
+			Orthographic, Prespective
+		};
+
 		class Camera
 		{
-		public:
-			Camera(Math::Mat4x4 viewMat, Math::Mat4x4 projMat);
+		protected:
+			Camera(Math::Mat4x4 viewMat, Math::Mat4x4 projMat, CameraType type, float aspectRatio, float orthoSizeOrFov);
 			~Camera() = default;
 
-			inline const Math::Mat4x4& GetViewMatrix() const { return m_ViewMat; }
-			inline const Math::Mat4x4& GetProjectionMatrix() const { return m_ProjectionMat; }
-			inline const Math::Mat4x4& GetViewProjectionMatrix() const { return m_ViewProjectionMat; }
+		public:
+			inline const Math::Mat4x4& GetViewMatrix() const { return m_ViewMatrix; }
+			inline const Math::Mat4x4& GetProjectionMatrix() const { return m_ProjectionMatrix; }
+			inline const Math::Mat4x4& GetViewProjectionMatrix() const { return m_ViewProjectionMatrix; }
 
-			inline void SetViewMatrix(const Math::Mat4x4 viewMatrix) { m_ViewMat = viewMatrix; }
-			inline void SetProjectionMatrix(const Math::Mat4x4 projMatrix) { m_ProjectionMat = projMatrix; }
+			inline void SetViewMatrix(const Math::Mat4x4& viewMatrix) { m_ViewMatrix = viewMatrix; RecalculateViewMatrix(); }
+			inline void SetProjectionMatrix(const Math::Mat4x4& projMatrix) { m_ProjectionMatrix = projMatrix; RecalculateProjectionMatrix(); }
 
+			inline const CameraType GetType() { return m_Type; }
+			inline const bool IsOrthographic() { return m_Type == CameraType::Orthographic; }
+			inline const bool IsPrespective() { return m_Type == CameraType::Prespective; }
+
+			inline const Math::Vector3& GetPosition() const { return m_Position; }
+			inline const Math::Vector3& GetRotation() const { return m_Rotation; }
+
+			inline void SetPosition(const Math::Vector3& position) { m_Position = position; RecalculateViewMatrix(); }
+			inline void SetRotation(const Math::Vector3& rotation) { m_Rotation = rotation; RecalculateViewMatrix(); }
+
+			inline void SetAspectRatio(float aspectRatio) { m_AspectRatio = aspectRatio; RecalculateProjectionMatrix(); }
+			inline const float& GetAspectRatio() { return m_AspectRatio; }
+
+			inline void SetOrthographicSize(float orthographicSize) { m_OrthographicSize = orthographicSize; RecalculateProjectionMatrix(); }
+			inline const float& GetOrthographicSize() { return m_OrthographicSize; }
+
+			inline void SetFieldOfView(float fov) { m_FieldOfView = fov; RecalculateProjectionMatrix(); }
+			inline const float& GetFieldOfView() { return m_FieldOfView; }
 		protected:
 			void RecalculateViewProjectionMatrix();
+			void RecalculateViewMatrix();
+			virtual void RecalculateProjectionMatrix() = 0;
 		protected:
-			Math::Mat4x4 m_ViewMat;
-			Math::Mat4x4 m_ProjectionMat;
-			Math::Mat4x4 m_ViewProjectionMat;
+			Math::Mat4x4 m_ViewMatrix;
+			Math::Mat4x4 m_ProjectionMatrix;
+			Math::Mat4x4 m_ViewProjectionMatrix;
+
+			Math::Vector3 m_Position;
+			Math::Vector3 m_Rotation;
+
+			CameraType m_Type;
+			float m_AspectRatio, m_FieldOfView, m_OrthographicSize;
+			union { float m_FieldOfView, m_OrthographicSize; };
 		};
 
 		class OrthographicCamera : public Camera
 		{
 		public:
-			OrthographicCamera(float size, float aspectRatio);
+			OrthographicCamera(float size, float aspectRatio, float zNear = -1.0f, float zFar = 1.0f);
 			~OrthographicCamera() = default;
+		protected:
+			virtual void RecalculateProjectionMatrix() override;
+		};
 
-			void SetOrthographicSize(float size);
-			void SetAspectRatio(float aspectRatio);
-
-			inline float GetOrthographicSize() { return m_Size; }
-			inline float GetAspectRatio() { return m_AspectRatio; }
-		private:
-			void RecalculateProjectionMatrix();
-		private:
-			float m_Size;
-			float m_AspectRatio;
+		class PerspectiveCamera : public Camera
+		{
+		public:
+			PerspectiveCamera(float fov, float aspectRatio, float zNear = 0.05f, float zFar = 100.0f);
+			~PerspectiveCamera() = default;
+		protected:
+			virtual void RecalculateProjectionMatrix() override;
 		};
 	}
 }
