eb20dfb1b5367dde50bfd3a3270a86f5ee0a8ab1
OverShifted <prowidgs@gmail.com>
2020-06-25 14:33:04 +0430

    CleanUp Init.cpp

--- OverEngine/src/OverEngine/Core/Init.cpp
+++ OverEngine/src/OverEngine/Core/Init.cpp
@@ -1,13 +1,13 @@
 #include "pcheader.h"
-
 #include "Init.h"
-#include "OverEngine/Input/InputSystem.h"
+
+#include "OverEngine/Core/Log.h"
 
 namespace OverEngine
 {
 	void Initialize()
 	{
-		OverEngine::Log::Init();
+		Log::Init();
 
 		#ifdef _MSC_VER
 			OE_CORE_INFO("OverEngine v0.0 [MSC {0}]", _MSC_VER);
