aaacfe2d58c675ee75af6fc303f3822419496375
OverShifted <prowidgs@gmail.com>
2020-07-29 17:28:20 +0430

    replace std::bind in OE_BIND_EVENT_FN macro

--- OverEngine/src/OverEngine/Core/Core.h
+++ OverEngine/src/OverEngine/Core/Core.h
@@ -100,7 +100,7 @@
 
 #define BIT(x) (1 << x)
 
-#define OE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define OE_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace OverEngine
 {
