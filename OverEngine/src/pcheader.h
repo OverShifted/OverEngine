166501efe4bb587653046158668db4de5f1204e9
OverShifted <prowidgs@gmail.com>
2020-08-23 16:35:01 +0430

    add a nice comment + .editorconfig

--- OverEngine/src/pcheader.h
+++ OverEngine/src/pcheader.h
@@ -1,7 +1,10 @@
 #pragma once
-
-#ifndef NOMINMAX
-#define NOMINMAX
+    
+#ifdef OE_PLATFORM_WINDOWS
+    #ifndef NOMINMAX
+        // See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
+        #define NOMINMAX
+    #endif
 #endif
 
 #include <iostream>
