689123d355ff78a0625ecf6d1822bfc1ed000522
Sepehr Kalanaki <prowidgs@gmail.com>
2023-01-05 12:00:18 +0330

    Build wren locally + Improve stb_image.cpp

--- OverEngine/vendor/stb/stb_image.cpp
+++ OverEngine/vendor/stb/stb_image.cpp
@@ -1,8 +1,8 @@
 #include "pcheader.h"
 
-#ifdef __GNUC__
-	#warning "Defining STBI_NO_SIMD because of spdlog (see https://github.com/gabime/spdlog/issues/2248)"
-	#define STBI_NO_SIMD
+#if defined(__GNUC__) && defined(OE_DEBUG) && defined(__OPTIMIZE__)
+	#warning "Undefing __OPTIMIZE__ because of fmt (see https://github.com/nothings/stb/discussions/1432)"
+	#undef __OPTIMIZE__
 #endif
 
 #define STB_IMAGE_IMPLEMENTATION
